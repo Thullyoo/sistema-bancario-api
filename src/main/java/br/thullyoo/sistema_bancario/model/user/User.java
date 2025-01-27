247d94ccf486d042f0cfc865e963a2729fcbea90
Thullyoo <thullyofilho@gmail.com>
2025-01-27 11:08:04 -0300

    feat: User Model class

---
+++ src/main/java/br/thullyoo/sistema_bancario/model/user/User.java
@@ -0,0 +1,84 @@
+package br.thullyoo.sistema_bancario.model.user;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class User {
+
+    private UUID uuid;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+    private String password;
+
+    @NotBlank(message = "Document cannot be blank")
+    @Pattern(regexp = "\\d{11}", message = "Document must be a valid 11-digit number")
+    private String document;
+
+    @NotNull(message = "Balance cannot be null")
+    @DecimalMin(value = "0.0", inclusive = true, message = "Balance must be a positive number")
+    private BigDecimal balance;
+
+    public User() {
+    }
+
+
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
