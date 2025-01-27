7be9b02f0ff7a2d5235d57937d052f4bf6cf51e7
Thullyoo <thullyofilho@gmail.com>
2025-01-27 12:11:06 -0300

    feat: add Transaction model

---
+++ src/main/java/br/thullyoo/sistema_bancario/model/user/Transaction.java
@@ -0,0 +1,60 @@
+package br.thullyoo.sistema_bancario.model.user;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Transaction {
+
+    private Long id;
+
+    private LocalDateTime issuedAt;
+
+    private String documentSender;
+
+    private String documentReceiver;
+
+    private BigDecimal value;
+
+    public Transaction() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getIssuedAt() {
+        return issuedAt;
+    }
+
+    public void setIssuedAt(LocalDateTime issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+
+    public String getDocumentSender() {
+        return documentSender;
+    }
+
+    public void setDocumentSender(String documentSender) {
+        this.documentSender = documentSender;
+    }
+
+    public String getDocumentReceiver() {
+        return documentReceiver;
+    }
+
+    public void setDocumentReceiver(String documentReceiver) {
+        this.documentReceiver = documentReceiver;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
