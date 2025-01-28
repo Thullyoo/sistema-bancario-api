fc18b7e6fff7099469ee32d38ede078a3f45be5b
Thullyoo <thullyofilho@gmail.com>
2025-01-28 12:25:04 -0300

    feat: add user repository and method to register user

---
+++ src/main/java/br/thullyoo/sistema_bancario/repository/UserRepository.java
@@ -0,0 +1,35 @@
+package br.thullyoo.sistema_bancario.repository;
+
+import br.thullyoo.sistema_bancario.model.user.User;
+import br.thullyoo.sistema_bancario.model.user.UserRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class UserRepository {
+
+    @Autowired
+    private JdbcClient jdbcClient;
+
+    public void register(UserRequest userRequest){
+        jdbcClient.sql("""
+                INSERT INTO USERS (name, email, password, balance, document)
+                VALUES (:name, :email, :password, :balance, :document)
+                """)
+                .param("name", userRequest.name())
+                .param("email", userRequest.email())
+                .param("password", userRequest.password())
+                .param("balance", new BigDecimal(0))
+                .param("document", userRequest.document())
+                .update();
+    }
+
+
+
+}
