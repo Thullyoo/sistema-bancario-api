1e002ccd1adb6d9b292e0222bb911a9b6e9e30f0
Thullyoo <thullyofilho@gmail.com>
2025-02-03 13:25:39 -0300

    feat: add dynamodb annotation and validation

--- src/main/java/br/thullyoo/sistema_bancario/model/transaction/Transaction.java
+++ src/main/java/br/thullyoo/sistema_bancario/model/transaction/Transaction.java
@@ -1,23 +1,41 @@
 package br.thullyoo.sistema_bancario.model.transaction;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@DynamoDbBean
 public class Transaction {
 
     private Long id;
 
+    @NotNull(message = "Issued date cannot be null")
     private LocalDateTime issuedAt;
 
+    @NotBlank(message = "Document sender cannot be blank")
+    @Size(min = 2, max = 100, message = "Document sender must be between 2 and 100 characters")
     private String documentSender;
 
+    @NotBlank(message = "Document receiver cannot be blank")
+    @Size(min = 2, max = 100, message = "Document receiver must be between 2 and 100 characters")
     private String documentReceiver;
 
+    @NotNull(message = "Value cannot be null")
+    @DecimalMin(value = "0.01", message = "Value must be greater than zero")
     private BigDecimal value;
 
     public Transaction() {
     }
 
+    @DynamoDbAttribute("transaction_id")
+    @DynamoDbPartitionKey
     public Long getId() {
         return id;
     }
@@ -26,6 +44,7 @@ public class Transaction {
         this.id = id;
     }
 
+    @DynamoDbAttribute("issuedAt")
     public LocalDateTime getIssuedAt() {
         return issuedAt;
     }
@@ -34,6 +53,7 @@ public class Transaction {
         this.issuedAt = issuedAt;
     }
 
+    @DynamoDbAttribute("sender_document")
     public String getDocumentSender() {
         return documentSender;
     }
@@ -42,6 +62,7 @@ public class Transaction {
         this.documentSender = documentSender;
     }
 
+    @DynamoDbAttribute("receiver_document")
     public String getDocumentReceiver() {
         return documentReceiver;
     }
@@ -50,6 +71,7 @@ public class Transaction {
         this.documentReceiver = documentReceiver;
     }
 
+    @DynamoDbAttribute("value")
     public BigDecimal getValue() {
         return value;
     }
