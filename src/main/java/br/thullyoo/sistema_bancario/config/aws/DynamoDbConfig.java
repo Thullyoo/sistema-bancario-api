dc646b7bd51d9de7fbe333dd2ad8ff0ef8088518
Thullyoo <thullyofilho@gmail.com>
2025-02-03 13:24:41 -0300

    feat: add dynamodb config

---
+++ src/main/java/br/thullyoo/sistema_bancario/config/aws/DynamoDbConfig.java
@@ -0,0 +1,25 @@
+package br.thullyoo.sistema_bancario.config.aws;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
+
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Bean
+    public DynamoDbClient dynamoDbClient(){
+        return DynamoDbClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.SA_EAST_1)
+                .build();
+    }
+
+}
