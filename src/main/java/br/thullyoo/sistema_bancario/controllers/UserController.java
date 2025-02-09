a34db1b7b7feaee8937aa221a8ffa799cf2f37e1
Thullyoo <thullyofilho@gmail.com>
2025-02-09 13:55:13 -0300

    feat: add user controller and method to increment balance user

--- src/main/java/br/thullyoo/sistema_bancario/controllers/UserController.java
+++ src/main/java/br/thullyoo/sistema_bancario/controllers/UserController.java
@@ -1,14 +1,17 @@
 package br.thullyoo.sistema_bancario.controllers;
 
+import br.thullyoo.sistema_bancario.model.user.IncrementRequest;
 import br.thullyoo.sistema_bancario.model.user.User;
 import br.thullyoo.sistema_bancario.model.user.UserRequest;
 import br.thullyoo.sistema_bancario.repository.UserRepository;
+import br.thullyoo.sistema_bancario.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @RestController
@@ -17,6 +20,10 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
 
     @PostMapping("/register")
     public ResponseEntity<Void> registerUser(@RequestBody @Valid UserRequest userRequest){
@@ -39,6 +46,16 @@ public class UserController {
         }
     }
 
+    @PostMapping("/increment")
+    public ResponseEntity<Void> incrementBalance(@RequestParam String document, @RequestBody IncrementRequest request){
+        try{
+            userService.incrementBalance(request.value(), document);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
 
 
 }
