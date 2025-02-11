54991c0255f4d7ca08e7f7b52d18820889a632f1
Thullyoo <thullyofilho@gmail.com>
2025-02-11 12:20:33 -0300

    feat: add endpoint to get all Transactions

--- src/main/java/br/thullyoo/sistema_bancario/controllers/TransactionController.java
+++ src/main/java/br/thullyoo/sistema_bancario/controllers/TransactionController.java
@@ -6,10 +6,9 @@ import br.thullyoo.sistema_bancario.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transaction")
@@ -22,5 +21,10 @@ public class TransactionController {
     public ResponseEntity<Transaction> register(@RequestBody TransactionRequest transactionRequest){
         Transaction transaction = transactionService.register(transactionRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(transaction);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions(){
+        return ResponseEntity.status(HttpStatus.OK).body(transactionService.getAllTransactions());
     }
 }
