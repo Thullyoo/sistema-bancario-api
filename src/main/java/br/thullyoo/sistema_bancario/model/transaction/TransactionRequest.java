03c86dca48a29895e11885dd9a81ea2cda6ea11b
Thullyoo <thullyofilho@gmail.com>
2025-02-20 12:46:46 -0300

    refactor: remove parameter no longer necessary

--- src/main/java/br/thullyoo/sistema_bancario/model/transaction/TransactionRequest.java
+++ src/main/java/br/thullyoo/sistema_bancario/model/transaction/TransactionRequest.java
@@ -3,5 +3,5 @@ package br.thullyoo.sistema_bancario.model.transaction;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record TransactionRequest(LocalDateTime issuedAt, String documentSender, String documentReceiver, BigDecimal value) {
+public record TransactionRequest(String documentReceiver, BigDecimal value) {
 }
