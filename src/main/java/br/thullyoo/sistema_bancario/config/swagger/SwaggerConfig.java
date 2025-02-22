ed97983ed4a023c255a0aa8ceee941a1e5ce38a2
Thullyoo <thullyofilho@gmail.com>
2025-02-22 15:16:20 -0300

    feat: add SecurityScheme on swagger config

--- src/main/java/br/thullyoo/sistema_bancario/config/swagger/SwaggerConfig.java
+++ src/main/java/br/thullyoo/sistema_bancario/config/swagger/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package br.thullyoo.sistema_bancario.config.swagger;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @Configuration
 public class SwaggerConfig {
 
