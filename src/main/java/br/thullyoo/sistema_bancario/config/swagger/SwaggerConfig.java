0fb4a984ca62b30291af270d4696aba1d56dfc09
Thullyoo <thullyofilho@gmail.com>
2025-02-22 14:58:17 -0300

    feat: add Swagger config

---
+++ src/main/java/br/thullyoo/sistema_bancario/config/swagger/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package br.thullyoo.sistema_bancario.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sistema bancario API")
+                        .version("1.0")
+                        .description("API que simula sistema de registro de transações entre usuários")
+                        .contact(new Contact()
+                                .email("thullyocontact@gmail.com")
+                                .name("Thúllyo Barcellos")
+                        )
+                );
+    }
+
+}
